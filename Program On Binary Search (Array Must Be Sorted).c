dadb39b99cd82467c00bc7713ac90a1707199962
R Raksha <154651032+Rakshar30@users.noreply.github.com>
2025-12-31 22:20:34 +0530

    Add binary search program in C

---
+++ Program On Binary Search (Array Must Be Sorted).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int arr[50], n, key, low = 0, high, mid;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter sorted array: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    high = n - 1;
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            printf("Element found at position %d", mid + 1);
+            return 0;
+        } else if (key < arr[mid]) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    printf("Element not found.");
+    return 0;
+}
